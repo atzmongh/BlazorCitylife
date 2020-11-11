f01282fc22e31f0b42972fb6050a39777900007c
Atzmon Ghilai <atzmon.ghilai@gmail.com>
2020-11-11 23:17:35 +0200

    login

---
+++ Data/CitylifeDBService.cs
@@ -0,0 +1,33 @@
+﻿using BlazorCitylife.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCitylife.Data
+{
+    public class CitylifeDBService
+    {
+        public Employee UserLoggedin(string userName, string password)
+        {
+            using(var db = new citylifedb8_blContext())
+            {
+                Employee theEmployee = db.Employee.SingleOrDefault(anEmp=>anEmp.UserName == userName);
+                if (theEmployee == null)
+                {
+                    return null;
+                }
+                else
+                {
+                    if (theEmployee.PasswordHash != password)
+                    {
+                        return null;
+                    }
+                }
+                //If we got here - we have an authenticated user
+                return theEmployee;
+            }
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -29,6 +29,7 @@ namespace BlazorCitylife
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddTransient<CitylifeDBService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
